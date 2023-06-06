9501352ab1547546a9719fb8a7de149d93b29d9e
ThomasCharuel <ThomasCharuel@users.noreply.github.com>
2023-06-06 16:58:55 +0200

    Implement employees page

---
+++ src/features/employees.js
@@ -0,0 +1,51 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  data: [
+    {
+      firstName: 'Emily',
+      lastName: 'Johnson',
+      startDate: '2019-03-15',
+      department: 'Sales',
+      dateOfBirth: '1987-09-12',
+      street: '123 Main Street',
+      city: 'Anytown',
+      state: 'California',
+      zipCode: 12345,
+    },
+    {
+      firstName: 'Ethan',
+      lastName: 'Wilson',
+      startDate: '2018-11-20',
+      department: 'Engineering',
+      dateOfBirth: '1992-07-18',
+      street: '321 Maple Lane',
+      city: 'Riversville',
+      state: 'Texas',
+      zipCode: 98765,
+    },
+    {
+      firstName: 'Olivia',
+      lastName: 'Thompson',
+      startDate: '2023-02-05',
+      department: 'Finance',
+      dateOfBirth: '1988-04-30',
+      street: '567 Pine Road',
+      city: 'Harborville',
+      state: 'Florida',
+      zipCode: 23456,
+    },
+  ],
+};
+
+export const employeesSlice = createSlice({
+  name: 'employees',
+  initialState,
+  reducers: {},
+});
+
+//export const {} = employeesSlice.actions;
+
+export const selectEmployees = (state) => state.employees.data;
+
+export default employeesSlice.reducer;
--- src/pages/Employees/index.jsx
+++ src/pages/Employees/index.jsx
@@ -1,11 +1,82 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import MUIDataTable from 'mui-datatables';
+import { selectEmployees } from '../../features/employees';
+
+const columns = [
+  {
+    name: 'firstName',
+    label: 'First Name',
+    options: {
+      sort: true,
+    },
+  },
+  {
+    name: 'lastName',
+    label: 'Last Name',
+    options: {
+      sort: true,
+    },
+  },
+  {
+    name: 'startDate',
+    label: 'Start Date',
+    options: {
+      sort: true,
+    },
+  },
+  {
+    name: 'department',
+    label: 'Department',
+    options: {
+      sort: true,
+    },
+  },
+  {
+    name: 'dateOfBirth',
+    label: 'Date of Birth',
+    options: {
+      sort: true,
+    },
+  },
+  {
+    name: 'street',
+    label: 'Street',
+    options: {
+      sort: true,
+    },
+  },
+  {
+    name: 'city',
+    label: 'City',
+    options: {
+      sort: true,
+    },
+  },
+  {
+    name: 'state',
+    label: 'State',
+    options: {
+      sort: true,
+    },
+  },
+  {
+    name: 'zipCode',
+    label: 'Zip Code',
+    options: {
+      sort: true,
+    },
+  },
+];
 
 export default function Employees() {
-  console.log('Hello');
+  const employees = useSelector(selectEmployees);
+  console.log(employees);
   return (
     <div id="employee-div" className="container">
       <h1>Current Employees</h1>
+      <MUIDataTable data={employees} columns={columns} />
       <table id="employee-table" className="display"></table>
       <Link to="/">Home</Link>
     </div>
